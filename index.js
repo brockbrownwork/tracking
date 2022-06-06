87830cf46c9efae07bdf0a5ae93477173d63973b
Hopper <Glenn.Hopper@signetjewelers.com>
2022-06-06 10:51:12 -0400

    Scanner now functional

--- index.js
+++ index.js
@@ -13,25 +13,52 @@
 // document.getElementsByTagName('head')[0].appendChild(script);
 
 const backgroundColor = "#B9B9B9";
+const alphanumeric =  'abcdefghijklmnopqrstuvwxyz0123456789';
+const minimumDelay = 300; // ms between keystrokes
 var lastKeystrokeTime = new Date().getTime();
+var keystrokeChunk = '';
 
 $("body").css("background-color", backgroundColor);
 
 document.onkeypress = function (e) {
+	// detects keypresses, when a burst of 6 keystrokes are detected with minimal delay followed by "enter"
     e = e || window.event;
-	let currentKeystrokeTime = new Date().getTime();
-	let deltaTime = currentKeystrokeTime - lastKeystrokeTime;
-	lastKeystrokeTime = currentKeystrokeTime;
-	console.log("Time: " + currentKeystrokeTime);
-	console.log("Delta time: " + deltaTime);
-	console.log(e);
-    // use e.keyCode
+	if (e.key && alphanumeric.includes(e.key)){
+		console.log('this is alphanumeric: ' + e.key);
+		let currentKeystrokeTime = new Date().getTime();
+		let deltaTime = currentKeystrokeTime - lastKeystrokeTime;
+		let keycode = e.key;
+		if (deltaTime < minimumDelay) {
+			keystrokeChunk += e.key;
+		console.log(`Keystroke chunk: ${keystrokeChunk}`);
+		}
+		else {
+			keystrokeChunk = e.key;
+		}
+		lastKeystrokeTime = currentKeystrokeTime;
+		// console.log("Time: " + currentKeystrokeTime);
+		console.log("Delta time: " + deltaTime);
+		// use e.keyCode
+	}
+	else if (e.key == "Enter") {
+		console.log("Enter pressed!");
+		let currentKeystrokeTime = new Date().getTime();
+		let deltaTime = currentKeystrokeTime - lastKeystrokeTime;
+		let keycode = e.key;
+		if (deltaTime < minimumDelay && keystrokeChunk.length > 5) {
+			console.log("Scan detected: " + keystrokeChunk);
+			scan(keystrokeChunk);
+		}
+		else {
+			keystrokeChunk = '';
+		}
+		lastKeystrokeTime = currentKeystrokeTime;
+	}
+	// else {
+		// console.log("Unhandled keystroke info:");
+		// console.log(e);
+	// }
 };
-
-// $("#thatButton").click(function(){
-	// $("#barcode").fadeOut();
-	// console.log("clicky clicky");
-// });
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -44,11 +71,12 @@ function choice(choices) {
 var visible = false;
 var firstScan = true;
 $("#barcode").hide()
+const fadeSpeed = 500; // ms
 async function scan(upc) {
 	// makes the upc appear then disappear
 	if (!firstScan){
-		$("#barcode").fadeOut();
-		await new Promise(r => setTimeout(r, 1000));
+		$("#barcode").fadeOut(fadeSpeed);
+		await new Promise(r => setTimeout(r, fadeSpeed + 10)); // don't remove this, fixes fades because of async shenanigans
 	}
 	else {
 		firstScan = false;
@@ -57,14 +85,14 @@ async function scan(upc) {
 								fontSize:20,
 								lineColor: "#0f2edb",
 								background: backgroundColor});
-	$("#barcode").fadeIn();
+	$("#barcode").fadeIn(fadeSpeed);
 }
 
 var repeat = function(){
+	// tests out the scan function
 	console.log("repeat");
-	let choices = ["test", "hello", "hello world", "hmm", "yup"]
+	let choices = ["test", "hello", "hello world", "hmm", "yup", "12345", "67890"]
 	scan(choice(choices));
 	// scan(Math.floor(1000000+Math.random()*9000000));
 };
-setInterval(repeat,5000);
-repeat();
+// setInterval(repeat,1000);
