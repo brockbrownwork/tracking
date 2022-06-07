0d86bed89c285a9da32d88afe0c3e72a04f99c50
Hopper <Glenn.Hopper@signetjewelers.com>
2022-06-07 08:12:32 -0400

    Adding comments, cleaning up

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const backgroundColor = "#B9B9B9";
+/*
+
+Tracking system frontend for Signet
+
+Scattered ideas:
+- undo scans? (maybe better to handle from server side, or maybe just scan to proper location instead)
+
+*/
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const allowedCharacters =  '0123456789' + alphabet + alphabet.toUpperCase();
 const minimumDelay = 300; // ms between keystrokes
+const minimumCharacters = 3; // this is the minimum length for a barcode scan (to prevent misfires)
 var lastKeystrokeTime = new Date().getTime();
 var keystrokeChunk = '';
+var locations = ["stamps", "cell 1", "cell 2", "shipping"];
 
+// count pageviews through localStorage, will get rid of this at some point
+var pageViews = localStorage.getItem("pageViews");
+if (pageViews) {
+		pageViews = (parseInt(pageViews) + 1).toString();
+		localStorage.setItem('pageViews', pageViews);
+}
+else {
+	pageViews = '1';
+	localStorage.setItem('pageViews', pageViews);
+}
+console.log(`Page views: ${pageViews}`)
+$("#page-views").html(`<u>Views:</u> <b>${pageViews}</b> <i>(test of localStorage)</i>`);
+
+// change the background color so that it matches the background of the barcode
+const backgroundColor = "#B9B9B9";
 $("body").css("background-color", backgroundColor);
 
 document.onkeypress = function (e) {
-	// detects keypresses, when a burst of 6 keystrokes are detected with minimal delay followed by "enter"
+	// when a burst of 3 or more keystrokes are detected with minimal delay followed by "enter", treat it as a scan
     e = e || window.event;
 	if (e.key && allowedCharacters.includes(e.key)){
 		console.log('this is allowed: ' + e.key);
@@ -32,7 +56,7 @@ document.onkeypress = function (e) {
 		let currentKeystrokeTime = new Date().getTime();
 		let deltaTime = currentKeystrokeTime - lastKeystrokeTime;
 		let keycode = e.key;
-		if (deltaTime < minimumDelay && keystrokeChunk.length > 5) {
+		if (deltaTime < minimumDelay && keystrokeChunk.length >= minimumCharacters) {
 			console.log("Scan detected: " + keystrokeChunk);
 			scan(keystrokeChunk);
 		}
@@ -60,18 +84,22 @@ var firstScan = true;
 $("#barcode").hide()
 const fadeSpeed = 500; // ms
 async function scan(upc) {
-	// makes the upc appear then disappear
-	if (!firstScan){
+	// makes the upc appear until the next one gets scanned
+	if (firstScan){
+		JsBarcode("#barcode", upc,{	displayValue:true,
+							fontSize:20,
+							lineColor: "#0f2edb",
+							background: backgroundColor});
+		firstScan = false;
+	}
+	else {
 		$("#barcode").fadeOut(fadeSpeed);
 		await new Promise(r => setTimeout(r, fadeSpeed + 10)); // don't remove this, fixes fades because of async shenanigans
 	}
-	else {
-		firstScan = false;
-	}
 	JsBarcode("#barcode", upc,{	displayValue:true,
-								fontSize:20,
-								lineColor: "#0f2edb",
-								background: backgroundColor});
+							fontSize:20,
+							lineColor: "#0f2edb",
+							background: backgroundColor});
 	$("#barcode").slideDown(fadeSpeed);
 }
 
